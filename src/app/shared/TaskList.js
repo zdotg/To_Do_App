0e410515adb9a55d7e7805c01f6c36a6fb3b70ce
Zach Gallman <zach.ecab@gmail.com>
2023-04-22 11:39:36 -0400

    Created 'TaskList' component

--- src/app/shared/TaskList.js
+++ src/app/shared/TaskList.js
@@ -1,2 +1,16 @@
 import React from "react";
 import Task from "./Task";
+
+function TaskList(props) {
+  const { tasks, onComplete } = props;
+
+  return (
+    <div className="task-list">
+      {tasks.map((task) => (
+        <Task key={task.id} task={task} onComplete={onComplete} />
+      ))}
+    </div>
+  );
+}
+
+export default TaskList;
